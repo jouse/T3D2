e0d86b6076a80de4a20f192e5fb59854734bdaa3
alan_wang <alan_wang@compal.com>
2020-01-30 00:13:02 +0800

    update t3d2

---
+++ Assets/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        Debug.Log("Level Won!");
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame()
+    {
+        if(gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game over.");
+            Invoke("Restart", 2f);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce;
+    public float sidewaysForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        //rb.AddForce(0, 0, 500 * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            forwardForce += 500;
+        }
+        else if(Input.GetKey(KeyCode.DownArrow))
+        {
+            forwardForce -= 500;
+        }
+
+        if(rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+
+        }
+    }
+}
